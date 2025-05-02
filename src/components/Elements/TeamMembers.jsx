4599e3b6b63126266f05221b6845a998c75d8964
PMYLS <faryalnimra190@gmail.com>
2025-05-02 17:08:39 +0500

    ADDED

--- src/components/Elements/TeamMembers.jsx
+++ src/components/Elements/TeamMembers.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-import { FaFacebook, FaLinkedin } from "react-icons/fa"; // Social icons
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./TeamMembers.scss"; // Custom styles
+import "./TeamMembers.scss";
 
 const teamData = [
   {
     id: 1,
-    name: "Mike Cannon-Brookes",
-    role: "AI Analyst",
-    image: "/assets/n.png",
-    bgColor: " #e0f7fa",
+    name: "Alina Gul",
+    role: "AI&Content Analyst",
+    image: "/assets/Member2.jpeg",
+    bgColor: "#e0f7fa",
     facebook: "https://www.facebook.com/profile.php?id=61574791244051",
-    linkedin: "#",
+    linkedin: "https://www.linkedin.com/in/alina-gul-8225aa28b",
   },
   {
     id: 2,
-    name: "Scott Farquhar",
-    role: "AI Analyst",
-    image: "/assets/n.png",
-    bgColor: " #e0f7fa",
+    name: "Faryal Nimra",
+    role: "Full Stack AI Developer",
+    image: "/assets/Member1.jpeg",
+    bgColor: "#e0f7fa",
     facebook: "https://www.facebook.com/profile.php?id=61574791244051",
-    linkedin: "#",
+    linkedin: "https://linkedin.com/in/faryal-nimra-4a49a32b6",
   },
   {
     id: 3,
-    name: "Anu Bharadwaj",
-    role: "AI Analyst",
-    image: "/assets/n.png",
-    bgColor: " #e0f7fa", 
+    name: "Maham Nadeem",
+    role: "Deep Learning Specialist",
+    image: "/assets/Member3.jpeg",
+    bgColor: "#e0f7fa",
     facebook: "https://www.facebook.com/profile.php?id=61574791244051",
-    linkedin: "#",
+    linkedin: "www.linkedin.com/in/maham-nadeem-070275267",
   },
 ];
 
@@ -39,23 +39,36 @@ const TeamMembers = () => {
       <h2 className="team-title text-center">Meet Our Leadership</h2>
       <div className="row justify-content-center">
         {teamData.map((member) => (
-          <div key={member.id} className="col-lg-3 col-md-4 col-sm-6 d-flex justify-content-center">
+          <div
+            key={member.id}
+            className="col-lg-3 col-md-4 col-sm-6 d-flex justify-content-center"
+          >
             <div
-              className="team-member text-center"
+              className="team-member"
               style={{ backgroundColor: member.bgColor }}
             >
               <div className="team-img-wrapper">
                 <img src={member.image} alt={member.name} className="team-img" />
-              </div>
-              <h3 className="team-name">{member.name}</h3>
-              <p className="team-role">{member.role}</p>
-              <div className="team-socials">
-                <a href={member.facebook} target="_blank" rel="noopener noreferrer">
-                  <FaFacebook />
-                </a>
-                <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
-                  <FaLinkedin />
-                </a>
+                <div className="team-info">
+                  <h3 className="team-name">{member.name}</h3>
+                  <p className="team-role">{member.role}</p>
+                  <div className="team-socials">
+                    <a
+                      href={member.facebook}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaFacebook />
+                    </a>
+                    <a
+                      href={member.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin />
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
